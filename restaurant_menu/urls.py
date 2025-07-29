e3e5f2a8577aef8f65539c6b6f8828386e9f872e
Ruxandra <filotiruxandra@gmail.com>
2025-07-29 17:17:33 +0300

    implemented admin interface and ui

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MenuList.as_view(), name='menu_list'),  # URL for the menu list page 
+    path('', views.MenuList.as_view(), name='home'),  # URL for the menu list page 
+    path('item/<int:pk>/', views.MenuDetail.as_view(), name="menu_item") #int:pk -> dynamic urls
 ]
 
